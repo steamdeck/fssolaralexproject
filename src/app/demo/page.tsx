9e6809eab6ea9d3cd9f337f7b775f2ecf940fb0c
Hardik Vaish <hardikvaish95@gmail.com>
2025-10-07 21:56:45 +0000

    create a /demo page and add this <!DOCTYPE html> <html> <head> <title>Pa

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,28 @@
+export default function DemoPage() {
+  const iframeStyle = {
+    position: 'fixed' as const,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    border: 'none',
+    margin: 0,
+    padding: 0,
+    overflow: 'hidden',
+    zIndex: 999999,
+  };
+
+  return (
+    <div>
+      <iframe
+        src="https://demo.casethemes.net/solarva/"
+        style={iframeStyle}
+        title="Solarva Demo"
+      >
+        Your browser doesn&apos;t support iframes
+      </iframe>
+    </div>
+  );
+}
