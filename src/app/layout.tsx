08b9470475b15bc909481c7b5726ba6648a2243b
Hardik Vaish <hardikvaish95@gmail.com>
2025-10-07 20:58:21 +0000

    add green color theme and gradient green bc this a solar panel website t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         )}
       >
         <Preloader />
-        <div className="relative flex min-h-dvh flex-col bg-background">
+        <div className="relative flex min-h-dvh flex-col bg-gradient-to-b from-background via-emerald-50/20 to-background">
           <SiteHeader />
           <main className="flex-1">{children}</main>
           <SiteFooter />
