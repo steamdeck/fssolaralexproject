d6ebb0bdc35ae7b0dfc728375c7dd924931ad53b
Hardik Vaish <hardikvaish95@gmail.com>
2025-11-01 10:59:45 +0000

    remove blog from header

--- src/components/layout/nav-links.ts
+++ src/components/layout/nav-links.ts
@@ -4,7 +4,6 @@ export const navLinks = [
   { href: '/about', label: 'About Us' },
   { href: '/services', label: 'Our Services' },
   { href: '/portfolio', label: 'Portfolio' },
-  { href: '/blog', label: 'Blog' },
   { href: '/calculator', label: 'Savings Calculator' },
   { href: '/contact', label: 'Contact Us' },
 ];
